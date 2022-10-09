d08951fa15896fabd1ce8a0ea1c43d5ca3f4af41
Nélia Simas <neliasimas@hotmail.com>
2022-10-09 03:32:35 -0300

    Create NotaEntreZeroEDez.java

---
+++ NotaEntreZeroEDez.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/* Pedir uma nota entrer 0 e 10.
+Se for um valor inválido, apresentar mensagem.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while (nota<0 | nota>10) {
+            System.out.println("Nota inválida! Digite novamente!");
+            nota = scan.nextInt();
+        }
+    }
+}
