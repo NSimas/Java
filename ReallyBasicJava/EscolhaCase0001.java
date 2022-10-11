60058728863689fb0a720971392fa5606cce197c
Nélia Simas <neliasimas@hotmail.com>
2022-10-11 02:58:14 -0300

    Create EscolhaCase0001.java

---
+++ ReallyBasicJava/EscolhaCase0001.java
@@ -0,0 +1,20 @@
+public class EscolhaCase0001 {
+    public static void escolha(String[] args) {
+        int codigoProduto = 2;
+                
+        switch(codigoProduto) [
+        // cada 'case' precisa do seu break.
+        case 1:
+            //bloco de comandos
+            break;
+            
+        case 2:
+            //bloco de comandos
+            break;
+            
+        default:
+            //bloco de comandos
+            
+                ]
+    }
+}
