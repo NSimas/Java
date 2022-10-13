7f0b01201f252aa612e5edf30a22bdef530f1e28
Nélia Simas <neliasimas@hotmail.com>
2022-10-13 19:05:08 -0300

    Create Repeticao0001.java

---
+++ ReallyBasicJava/Repeticao0001.java
@@ -0,0 +1,24 @@
+package ReallyBasicJava;
+
+import java.util.Scanner;
+
+public class Repeticao0001 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+            int totalAlunos = 10;
+
+        while(totalAlunos > 0){
+
+            String nomeAluno = leia.nextLine();
+            int idadeAluno = leia.nextInt();
+
+            System.out.println("O nome do aluno é: " +
+                    nomeAluno + " e sua idade é: " +
+                    idadeAluno);
+
+            totalAlunos = totalAlunos - 1;
+        }
+
+    }
+}
