f1f5a6d817c7c2809a568311d8aa487955174588
Nélia Simas <neliasimas@hotmail.com>
2022-10-25 04:08:48 -0300

    Updates

---
+++ ReallyBasicJava/ExerciciosDeArrays/Consooantes.java
@@ -0,0 +1,9 @@
+package ReallyBasicJava.ExerciciosDeArrays;
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+public class Consooantes {
+    
+}
