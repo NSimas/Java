ae2751c8b7deb4878c2232170fabf591ecdd6d4d
Nélia Simas <neliasimas@hotmail.com>
2022-10-25 21:56:29 -0300

    adds

---
+++ ReallyBasicJava/Metodos/Quadrilatero0001/Main.java
@@ -0,0 +1,14 @@
+package ReallyBasicJava.Metodos.Quadrilatero0001;
+
+/*
+ * Crie uma aplicação que calcule a área dos
+ * três quadriláteros notáveis: quadrado,
+ * retângulo e trapézio.
+ * Com uso de sobrecarga de métodos.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        aaaaaaaaaaaal
+    }
+}
