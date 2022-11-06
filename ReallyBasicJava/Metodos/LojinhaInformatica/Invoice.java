3071caeb0d02ca0227ae5d096e8cebaed651b70a
Nélia Simas <neliasimas@hotmail.com>
2022-11-06 12:12:29 -0300

    updates

--- ReallyBasicJava/Metodos/LojinhaInformatica/Invoice.java
+++ ReallyBasicJava/Metodos/LojinhaInformatica/Invoice.java
@@ -21,38 +21,56 @@ public class Invoice {
         private int quantidade;
         private float precoUnitario;
 
-        public void setCodigoItem (int codigoItem) {
+        public Invoice (int codigoItem, String descricao, int quantidade, float precoUnitario) {
+            this.setCodigoItem(codigoItem);
+            this.setDescricao(descricao);
+            this.setQuantidade(quantidade);
+            this.setPrecoUnitario(precoUnitario);
+        }
+
+        public double getInvoiceAmount() {
+            return; quantidade * precoUnitario;
+        }
+
+        public void setCodigoItem ( int codigoItem){
             this.codigoItem = codigoItem;
         }
-    
-        public float getCodigoItem() {
+
+        public float getCodigoItem () {
             return codigoItem;
         }
 
-        public void setDescricao (String descricao) {
+        public void setDescricao (String descricao){
             this.descricao = descricao;
         }
-    
-        public float getDescricao() {
+
+        public float getDescricao () {
             return descricao;
         }
 
-        public void setQuantidade (int quantidade) {
-            this.quantidade = quantidade;
+        public void setQuantidade ( int quantidade){
+
+            if (quantidade < 0) {
+                this.quantidade = 0;
+            } else {
+                this.quantidade = quantidade;
+            }
         }
-    
-        public float getQuantidade() {
+
+        public float getQuantidade () {
             return quantidade;
         }
 
-        public void setPrecoUnitario (float precoUnitario) {
-            this.precoUnitario = precoUnitario;
-        }
-    
-        public float getPrecoUnitario() {
-            return precoUnitario;
+        public void setPrecoUnitario ( float precoUnitario){
+
+            if (precoUnitario < 0) {
+                this.precoUnitario = 0;
+            } else {
+                this.precoUnitario = precoUnitario;
+            }
         }
 
-        
+        public float getPrecoUnitario () {
+            return precoUnitario;
+        }
     }
-}
