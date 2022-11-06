a65ff76c6d5fb69dfe901a229a1969f106121fe1
Nélia Simas <neliasimas@hotmail.com>
2022-11-06 12:27:29 -0300

    updates

--- ReallyBasicJava/Metodos/LojinhaInformatica/Main.java
+++ ReallyBasicJava/Metodos/LojinhaInformatica/Main.java
@@ -6,4 +6,10 @@ public class Main {
     
     Invoice i = new Invoice(1, "mousepad", 10, 48.0f);
     System.out.println("Valor total = " + i+getInvoiceAmount());
+
+    Invoice i = new Invoice(1, "mouse", 15, -6f);
+    System.out.println("Valor total = " + i+getInvoiceAmount());
+
+    Invoice i = new Invoice(1, "mousepad", -12, 48.0f);
+    System.out.println("Valor total = " + i+getInvoiceAmount());
 }
